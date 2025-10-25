8290bbe83b11b9ecc174c393564b8ebc76bbe4c7
gta-gta <studybuddywithfun@gmail.com>
2025-10-25 23:34:30 +0530

    push

--- bday-animation-main/vid.js
+++ bday-animation-main/vid.js
@@ -1,11 +1,16 @@
 const button = document.querySelector('.expandbtn');
 const videosect = document.querySelector('.videosec');
-
-
+const audios=document.querySelectorAll('audio');
 button.addEventListener('click',()=>{
     videosect.classList.toggle('active');
     window.scrollBy({
         top: 400, // Scroll up by 200 pixels
         behavior: 'smooth',// Smooth scrolling animation
     });
+})
+
+button.addEventListener('click',()=>{
+    audios.forEach((audio)=>{
+        audio.pause();
+    })
 })
